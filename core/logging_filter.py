b76824dd68a711a5f668593e64960079acbd127d
liuzhenghua <1090179900@qq.com>
2025-01-20 09:41:16 +0000

    fix tz timezone format issue

--- core/logging_filter.py
+++ core/logging_filter.py
@@ -1,11 +1,19 @@
 import logging
+import pytz
+import functools
+from datetime import datetime, timezone
 
 from fastapi import FastAPI
 
 from config import app_config
 
 
+def _time_converter(seconds, tz):
+    return datetime.fromtimestamp(seconds, tz=timezone.utc).astimezone(tz).timetuple()
+
+
 def init_app(app: FastAPI):
+    # add logging filter
     logging_filter = LoggingFilter(app)
     logger = logging.getLogger()
     logger.addFilter(logging_filter)
@@ -13,6 +21,13 @@ def init_app(app: FastAPI):
     loggers = [logging.getLogger(name) for name in logging.root.manager.loggerDict]
     for logger in loggers:
         logger.addFilter(logging_filter)
+
+    # format log timezone
+    log_tz = app_config.LOG_TZ
+    if log_tz:
+        tz = pytz.timezone(log_tz)
+        for handler in logging.root.handlers:
+            handler.formatter.converter = functools.partial(_time_converter, tz=tz)
 
 class LoggingFilter(logging.Filter):
 
